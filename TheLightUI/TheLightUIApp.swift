6ae2e228fe8650910e9546dfbc26a169362850a0
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2021-08-29 14:43:03 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Stanislav Lemeshaev on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
